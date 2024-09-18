f2bb7b97b4234b7bf541d76bc5b87114fb0e005e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-09-18 16:16:20 +0200

    root element

--- projects/shell/src/components/Root/index.tsx
+++ projects/shell/src/components/Root/index.tsx
@@ -1,14 +1,15 @@
-import { Html } from '@elysiajs/html'
+import Html from '@elysiajs/html'
+console.log(Html)
 
 export default function Root(
-    { children }: 
+    { children }: { children: Html.Children }
 ) {
     return (
         <html lang="en">
             <head>
                 <meta charset="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Document</title>
+                <title>Archelon</title>
             </head>
             <body>
                 { children }
